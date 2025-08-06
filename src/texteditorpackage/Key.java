2d1dd352a804b739cce57e541a7694e5720483e9
Mitchell Lagaras <mookielagaras@gmail.com>
2025-08-05 20:36:40 -0400

    Create initial Keys.java

---
+++ src/texteditorpackage/Key.java
@@ -0,0 +1,27 @@
+package texteditorpackage;
+
+public class Key {
+  private char output;
+  private int length;
+
+  public Key(char output, int length) {
+    this.output = output;
+    this.length = length;
+  }
+
+  public char getOutput() {
+    return output;
+  }
+
+  public int getLength() {
+    return length;
+  }
+
+  public void setOutput() {
+    this.output = output;
+  }
+
+  public void setLength() {
+    this.length = length;
+  }
+}
---
+++ src/texteditorpackage/Keys.java
@@ -0,0 +1,36 @@
+package texteditorpackage;
+
+public class Keys() {
+  private ArrayList<Key> defArray;
+
+  public Keys() {
+    super();
+    defArray = new ArrayList<Key>(100);
+    defArray.add(new Key("a", 1));
+    defArray.add(new Key("b", 1));
+    defArray.add(new Key("c", 1));
+    defArray.add(new Key("d", 1));
+    defArray.add(new Key("e", 1));
+    defArray.add(new Key("f", 1));
+    defArray.add(new Key("g", 1));
+    defArray.add(new Key("h", 1));
+    defArray.add(new Key("i", 1));
+    defArray.add(new Key("j", 1));
+    defArray.add(new Key("k", 1));
+    defArray.add(new Key("l", 1));
+    defArray.add(new Key("m", 1));
+    defArray.add(new Key("n", 1));
+    defArray.add(new Key("o", 1));
+    defArray.add(new Key("p", 1));
+    defArray.add(new Key("q", 1));
+    defArray.add(new Key("r", 1));
+    defArray.add(new Key("s", 1));
+    defArray.add(new Key("t", 1));
+    defArray.add(new Key("u", 1));
+    defArray.add(new Key("v", 1));
+    defArray.add(new Key("w", 1));
+    defArray.add(new Key("x", 1));
+    defArray.add(new Key("y", 1));
+    defArray.add(new Key("z", 1));
+  }
+}
